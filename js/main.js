8e2f753c36f0e2327a2ca3e7b5df5b749de1c0c9
gabatk <gabriela.trybuszek@gmail.com>
2022-12-30 22:13:23 +0100

    Second JS solution implemented.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,53 @@
+// const countBtn = document.querySelector('.count');
+// const formFields = document.querySelectorAll('.form-field');
+// const billPerPerson = document.querySelector('.cost');
+// const costInfo = document.querySelector('.cost-info');
+// const errorField = document.querySelector('.error');
+
+// const tipValue = () => {
+// 	let billValue = Number(formFields[0].value);
+// 	let people = Number(formFields[1].value);
+// 	let tipRate = Number(formFields[2].value);
+
+// 	if (billValue != 0 && people != 0 && tipRate != 0) {
+// 		billPerPerson.textContent = (
+// 			(billValue + billValue * tipRate) /
+// 			people
+// 		).toFixed(2);
+// 		costInfo.style.display = 'block';
+// 		errorField.textContent = '';
+// 	} else {
+// 		errorField.textContent = 'All fields must be completed!';
+// 	}
+// };
+
+// countBtn.addEventListener('click', tipValue);
+
+// Rozwiązanie 2.
 const countBtn = document.querySelector('.count');
-const formFields = document.querySelectorAll('.form-field');
+const price = document.querySelector('#price');
+const people = document.querySelector('#people');
+const tip = document.querySelector('#tip');
 const billPerPerson = document.querySelector('.cost');
 const costInfo = document.querySelector('.cost-info');
-const errorField = document.querySelector('.error');
+const error = document.querySelector('.error');
 
 const tipValue = () => {
-	let billValue = Number(formFields[0].value);
-	let people = Number(formFields[1].value);
-	let tipRate = Number(formFields[2].value);
-
-	if (billValue != 0 && people != 0 && tipRate != 0) {
-		billPerPerson.textContent = (
-			(billValue + billValue * tipRate) /
-			people
-		).toFixed(2);
-		costInfo.style.display = 'block';
-		errorField.textContent = '';
+	if (price.value == '' || people.value == '' || tip.value == 0) {
+		error.textContent = 'All fields must be completed!';
 	} else {
-		errorField.textContent = 'All fields must be completed!';
+		error.textContent = '';
+		countBill();
 	}
+};
+
+const countBill = () => {
+	const billValue = parseFloat(price.value);
+	const peopleNumber = parseInt(people.value);
+	const tipRate = parseFloat(tip.value);
+
+	billPerPerson.textContent = ((billValue + billValue * tipRate) /peopleNumber).toFixed(2);
+	costInfo.style.display = 'block';
 };
 
 countBtn.addEventListener('click', tipValue);
